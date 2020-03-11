cb3263955eaac949055bc2b718a1062c69ea9d09
DanBaisal <dan.baisal92@gmail.com>
2020-03-11 22:52:39 +0100

    Added the wikipedia api

--- src/app/searchbar/searchbar.component.ts
+++ src/app/searchbar/searchbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-searchbar',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./searchbar.component.css']
 })
 export class SearchbarComponent implements OnInit {
+  @Output() submitted = new EventEmitter<string>(); // Generic annotation
+
   term = '';
 
   constructor() { }
@@ -17,4 +19,10 @@ export class SearchbarComponent implements OnInit {
     this.term = value;
   }
 
+  onFormSubmit(event: any) {
+    event.preventDefault();
+    this.submitted.emit(this.term);
+  }
 }
+
+// In order to communicate from a child to a parent component, we use EventEmitter and Output. 
