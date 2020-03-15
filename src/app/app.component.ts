5e391914123f2839a3d9b139fa93eea8c162a15b
DanBaisal <dan.baisal92@gmail.com>
2020-03-15 22:26:30 +0100

    Added dependency injection and created some front-end for the data request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,19 @@ import { WikipediaService } from './wikipedia.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  pages = []; // When the app first fires up, nothing is shown, therefore an empty array
+
   constructor(private wikipedia: WikipediaService) {
 
   }
 
   onTerm(term: string) {
-    const results = this.wikipedia.search(term);
-    console.log(results);
+    // const results = this.wikipedia.search(term);
+    // console.log(results);
+
+    this.wikipedia.search(term).subscribe((response: any) => {
+      this.pages = response;
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'; // Important for our request to work
 
 import { AppComponent } from './app.component';
 import { SearchbarComponent } from './searchbar/searchbar.component';
@@ -12,7 +13,8 @@ import { PageListComponent } from './page-list/page-list.component';
     PageListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
